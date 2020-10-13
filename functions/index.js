fe8dd61a4b0298cc2f85dcca3e79ea4b60e4d3f7
Alison Reilly <reillyamr@gmail.com>
2020-10-13 12:55:25 -0400

    annotate and initialize admin instance

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+// imported already
 const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
@@ -7,3 +9,8 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
