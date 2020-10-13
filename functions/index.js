4b20101f34ce432b95b605b965f9123f3f035f89
Alison Reilly <reillyamr@gmail.com>
2020-10-13 12:59:09 -0400

    annotate and addMessage function

--- functions/index.js
+++ functions/index.js
@@ -14,3 +14,16 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+//addMessage example
+// This example takes the text parameter passed to this HTTP endpoint and inserts it into 
+// Cloud Firestore under the path /messages/:documentId/original
+// further doc description: "The addMessage() function is an HTTP endpoint. Any request to
+// the endpoint results in ExpressJS-style Request and Response objects passed to the onRequest() callback."
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
